56ef8020c580713e2eaf9943b54b587ccab0caac
Olga <minaeva9@gmail.com>
2024-07-19 15:30:49 -0400

    grid container Applayout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,25 +1,26 @@
-import { Outlet, useNavigation } from "react-router-dom";
-import CartOverview from "../features/cart/CartOverview";
-import Header from "./Header";
-import Loader from "./Loader";
+import { Outlet, useNavigation } from 'react-router-dom'
+import CartOverview from '../features/cart/CartOverview'
+import Header from './Header'
+import Loader from './Loader'
 
 function AppLayout() {
-  const navigation = useNavigation();
-  const isLoading = navigation.state === "loading";
-  console.log(navigation);
-  return (
-    <div className="layout">
-      {isLoading && <Loader />}
+    const navigation = useNavigation()
+    const isLoading = navigation.state === 'loading'
 
-      <Header />
+    return (
+        <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+            {isLoading && <Loader />}
 
-      <main>
-        <Outlet />
-      </main>
+            <Header />
+            <div className="overflow-scroll">
+                <main className="mx-auto max-w-3xl">
+                    <Outlet />
+                </main>
+            </div>
 
-      <CartOverview />
-    </div>
-  );
+            <CartOverview />
+        </div>
+    )
 }
 
-export default AppLayout;
+export default AppLayout
