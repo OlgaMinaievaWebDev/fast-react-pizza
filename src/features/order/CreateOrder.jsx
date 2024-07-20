52e37285f9b5e37bb66d6f6802dd0381aae83c0e
Olga <minaeva9@gmail.com>
2024-07-20 14:51:58 -0400

    reusing styles with components

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom'
 import { createOrder } from '../../services/apiRestaurant'
-
+import Button from '../../ui/Button'
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
     /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
@@ -100,12 +100,9 @@ function CreateOrder() {
                         name="cart"
                         value={JSON.stringify(cart)}
                     />
-                    <button
-                        disabled={isSubmitting}
-                        className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
-                    >
+                    <Button disabled={isSubmitting}>
                         {isSubmitting ? 'Placing order...' : 'Order now'}
-                    </button>
+                    </Button>
                 </div>
             </Form>
         </div>
