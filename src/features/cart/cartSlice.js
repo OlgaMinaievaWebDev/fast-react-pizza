18b103e6dfc7a23977e5e92b0380634240dfee82
Olga <minaeva9@gmail.com>
2024-07-31 15:41:09 -0400

    cart

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
     // cart: [],
-    card: [
+    cart: [
         {
             pizzaId: 12,
             name: 'Mediterranean',
